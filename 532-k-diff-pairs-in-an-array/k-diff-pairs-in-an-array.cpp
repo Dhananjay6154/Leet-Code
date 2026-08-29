9e64492852b9b2569eb77401aab2fa05e1d0501f
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-08-30 01:33:08 +0530

    Time: 2 ms (79.80%) | Memory: 15.1 MB (83.90%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -2,24 +2,54 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        
         int ans = 0;
-
         for(int i=0;i<nums.size();i++)
         {
+            int target = nums[i]+k;
             if(i!=0 && nums[i] == nums[i-1])
             {
                 continue;
             }
-            for(int j=i+1;j<nums.size();j++)
+            int left = i+1;
+            int right = nums.size()-1;
+            int mid = left+(right-left) / 2;
+            while(left <= right)
             {
-                if(nums[j] - nums[i] == k)
+                int mid = left+(right-left) / 2;
+
+                if(nums[mid] == target)
                 {
                     ans++;
                     break;
                 }
+                else if(nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
             }
-        }    
-        return ans;    
+        }
+        return ans;
+        
+        // int ans = 0;
+
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(i!=0 && nums[i] == nums[i-1])
+        //     {
+        //         continue;
+        //     }
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[j] - nums[i] == k)
+        //         {
+        //             ans++;
+        //             break;
+        //         }
+        //     }
+        // }    
+        // return ans;    
     }
 };
