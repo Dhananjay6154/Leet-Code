4686f2a3868ab1f5d6218b40d34d178cea829034
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-09-21 16:26:31 +0530

    Time: 3 ms (13.72%) | Memory: 52.1 MB (62.66%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end());
-        int mini = prices[0];
-        int secMINI = prices[1];
+        int mini = INT_MAX;
+        int secMINI = INT_MAX;
         int total = 0;
 
+        for(int i = 0; i < prices.size(); i++)
+        {
+            if(prices[i] < mini)
+            {
+                secMINI = mini;
+                mini = prices[i];
+            }
+            else if(prices[i] < secMINI)
+            {
+                secMINI = prices[i];
+            }
+        }
         total = secMINI + mini;
         if(total > money)
         {
             return money;
         }
-        return money-total;
-
+        return money - total;
         
     }
 };
