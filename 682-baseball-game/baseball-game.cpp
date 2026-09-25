59306feeca9ab6d72be656e7338db766cc18984b
Dhananjay Purohit <dhruvpurohit6154@gmail.com>
2026-09-25 17:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (80.53%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> st;
+        for(auto op : operations)
+        {
+            if(op == "C")
+            {
+                st.pop_back();
+            }
+            else if(op == "D")
+            {
+                st.push_back(2 * st.back());
+            }
+            else if(op == "+")
+            {
+                int n = st.size();
+                st.push_back(st[n-1] + st[n-2]);
+            }
+            else{
+                st.push_back(stoi(op));
+            }
+        }
+        int sum = 0;
+        for(auto x : st)
+        {
+            sum += x;
+        }
+        return sum;
+        
+    }
+};
